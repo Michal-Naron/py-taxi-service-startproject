966a0d8b374c8646db8b0ef21604e097487495d2
Michal-Naron <michanaron@gmail.com>
2025-08-12 21:24:57 +0200

    add task solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length = 255, unique = True)
@@ -16,7 +17,8 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE,
                                     related_name="cars")
-    drivers = models.ManyToManyField("Driver")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                    related_name="cars")
 
     def __str__(self):
         return f"{self.manufacturer.name} {self.model}"
